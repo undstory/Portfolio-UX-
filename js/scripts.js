c58d5d9dc0e048f417460870c483f0fc50b88411
undstory <undrul.ozog@gmail.com>
2022-05-07 14:36:51 +0200

    a11y

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
 const toggler = document.querySelector(".hero__toggler--icon");
 const mobileNav = document.querySelector(".hero__mobile");
+const scrollToTopBtn = document.querySelector(".top-btn");
+
+const rootElement = document.documentElement;
+function handleScroll() {
+  let scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.1) {
+    // Show button
+    scrollToTopBtn.classList.add("show-top-btn");
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("show-top-btn");
+  }
+}
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
 
 toggler.addEventListener("click", () => {
   console.log("Hello");
